3a8fba5059eeab5fdff9c71bf355d61a5010c3e7
batikanyeni <batikanyeni@outlook.com>
2024-11-09 23:26:36 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
 import AboutMe from "./components/about-me/AboutMe";
+import BrowserRouter from 'react-router-dom/BrowserRouter'
 
 function App() {
   return (
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <AboutMe/>
+      </BrowserRouter>
+
   );
 }
 
