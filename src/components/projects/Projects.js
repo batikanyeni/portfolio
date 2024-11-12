e70db342204b6cfb27b6a682a5007190799c18a6
batikanyeni <batikanyeni@outlook.com>
2024-11-12 14:26:35 +0300

    safari potential fix 2

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -85,7 +85,7 @@ const Projects = () => {
                     </div>
                     <Swiper
                         slidesPerView={1}
-                        className="w-full rounded shadow-lg sm:h-96 hover:cursor-pointer"
+                        className="w-full rounded shadow-lg max-h-96 hover:cursor-pointer"
                         pagination={{ clickable: true }}
                         navigation={true}
                         modules={[Navigation,pgntn]}
